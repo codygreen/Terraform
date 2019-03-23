8c335a490f0789e5de03836b1a5771fc3a034ca3
Cody Green <cody@codygreen.com>
2019-03-23 00:18:20 -0500

    fixed string issue

--- aws/compute/get_ecs_ips.sh
+++ aws/compute/get_ecs_ips.sh
@@ -1,7 +1,7 @@
-TASKS=`aws ecs list-tasks --cluster f5-demo-httpd --output json \
+TASKS=`aws ecs list-tasks --cluster ecs-f5-demo --output json \
 | jq -r '.taskArns[]'`
-IPS=`aws ecs describe-tasks --cluster f5-demo-httpd --tasks \
+IPS=`aws ecs describe-tasks --cluster ecs-f5-demo --tasks \
 $TASKS | \
-jq '[ .tasks[].containers[].networkInterfaces[].privateIpv4Address | .] | join(",")' `
+jq -r '[ .tasks[].containers[].networkInterfaces[].privateIpv4Address | .] | join(",")' `
 
 jq -n --arg ips "$IPS" '{"ips": $ips}'
