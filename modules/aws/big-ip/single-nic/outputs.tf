18b50cf8b4d6fe7fbdc25652e864258e64461745
Cody Green <cody@codygreen.com>
2019-04-23 13:22:34 -0500

    added private_ip to output

--- modules/aws/big-ip/single-nic/outputs.tf
+++ modules/aws/big-ip/single-nic/outputs.tf
@@ -3,6 +3,10 @@ output "public_ip" {
   value = "${join(", ", aws_instance.f5_bigip.*.public_ip)}"
 }
 
+output "private_ip" {
+  value = "${join(", ", aws_instance.f5_bigip.*.private_ip)}"
+}
+
 output "password" {
-    value = "${random_string.password.result}"
+  value = "${random_string.password.result}"
 }
