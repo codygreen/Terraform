a2711fa7cefb1300a3b4c25939c4fee1ec5676c0
Cody Green <cody@codygreen.com>
2019-04-23 16:00:55 -0500

    no message

--- modules/aws/big-ip/single-nic/main.tf
+++ modules/aws/big-ip/single-nic/main.tf
@@ -24,18 +24,21 @@ data "aws_iam_policy_document" "assume_role_doc" {
       "sts:AssumeRole",
     ]
 
-    # principals = {
-    #   type        = "Service"
-    #   identifiers = ["ec2.amazonaws.com"]
-    # }
+    principals = {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
 
     effect = "Allow"
+
     sid = ""
   }
 }
 
-data "aws_iam_policy_document" "serviceDiscovery" {
+data "aws_iam_policy_document" "f5_ha_policy_doc" {
   statement {
+    effect = "Allow"
+
     actions = [
       "ec2:DescribeInstances",
       "ec2:DescribeInstanceStatus",
@@ -50,31 +53,31 @@ data "aws_iam_policy_document" "serviceDiscovery" {
       "sts:AssumeRole",
     ]
 
-    effect = "Allow"
-
-    sid = ""
+    resources = [
+      "*",
+    ]
   }
 }
 
-resource "aws_iam_policy" "bigip" {
-  name   = "${var.name}_bigip"
+resource "aws_iam_policy" "f5_ha_policy" {
+  name   = "f5_ha_policy"
   path   = "/"
-  policy = "${data.aws_iam_policy_document.assume_role_doc.json}"
+  policy = "${data.aws_iam_policy_document.f5_ha_policy_doc.json}"
 }
 
-resource "aws_iam_role" "bigip" {
-  name               = "${var.name}_assume_role"
+resource "aws_iam_role" "f5_ha" {
+  name               = "f5_ha"
   assume_role_policy = "${data.aws_iam_policy_document.assume_role_doc.json}"
 }
 
-resource "aws_iam_role_policy_attachment" "bigip" {
-  role       = "${aws_iam_role.bigip.name}"
-  policy_arn = "${aws_iam_policy.bigip.arn}"
+resource "aws_iam_role_policy_attachment" "f5_ha_attach" {
+  role       = "${aws_iam_role.f5_ha.name}"
+  policy_arn = "${aws_iam_policy.f5_ha_policy.arn}"
 }
 
 resource "aws_iam_instance_profile" "f5_profile" {
-  name = "${var.name}_f5_profile"
-  role = "${aws_iam_role.bigip.name}"
+  name = "f5_profile"
+  role = "${aws_iam_role.f5_ha.name}"
 }
 
 # Deploy BIG-IP
