29fd5cb28b648fb13e40eedb5290339133864323
Cody Green <cody@codygreen.com>
2019-04-23 14:46:08 -0500

    added ip output

--- modules/aws/compute-ec2/output.tf
+++ modules/aws/compute-ec2/output.tf
@@ -1,2 +1,8 @@
-#-------- compute/output.tf --------
+#-------- compute-ec2/output.tf --------
+output "public_ip" {
+  value = "${join(", ", aws_instance.nginx.*.public_ip)}"
+}
 
+output "private_ip" {
+  value = "${join(", ", aws_instance.nginx.*.private_ip)}"
+}
